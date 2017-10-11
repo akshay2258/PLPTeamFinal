b390a3d31f9b121fd571893c4f2a16f4c8ba6ee1
akashkum <akash.l.kumar@capgemini.com>
2017-10-11 08:31:15 -0400

    Web Layer

---
+++ PLP_UniversityAddmissionSystem_Team4/src/com/cg/uas/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.cg.uas.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
+import com.capgemini.uas.service.IAdminService;
+
+@Controller
+public class AdminController {
+	@Resource
+	IAdminService service;
+}
