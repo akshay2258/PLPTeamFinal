fd68985265d4bdddf52401c9ee318a385f2b98a1
akspaliw <akshay.paliwal@capgemini.com>
2017-10-19 08:07:45 +0530

    empty

--- PLP_UniversityAddmissionSystem_Team4/WebContent/jsLib/MyJsLib.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(
-		function(){
-			$('h1').css({"color":"green"});
-			$('a').css({"color":"blue"});
-			$("table tr:odd").css({"background-color":"lightblue"});
-			$("table tr:even").css({"background-color":"lightgreen"})
-		}
-
-)
--- PLP_UniversityAddmissionSystem_Team4/src/com/capgemini/uas/service/IUsersService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.uas.service;
-
-import com.capgemini.uas.dto.UsersBean;
-import com.capgemini.uas.exception.UniversityException;
-
-public interface IUsersService {
-
-	public String checkUser(UsersBean userBeanMain) throws UniversityException;
-}
